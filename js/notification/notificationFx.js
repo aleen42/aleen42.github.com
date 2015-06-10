944ba8998bceb855d64d2f71fc1b2d0975ef6db0
Aleen <408503598@qq.com>
2015-06-10 23:49:49 +0800

    update

--- js/notification/notificationFx.js
+++ js/notification/notificationFx.js
@@ -66,7 +66,7 @@
 		type : 'error',
 		// if the user doesn´t close the notification then we remove it 
 		// after the following time
-		ttl : 6000,
+		ttl : 15000,
 		// callbacks
 		onClose : function() { return false; },
 		onOpen : function() { return false; }
