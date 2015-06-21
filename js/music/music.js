95b438841ce34a65a40af5fbf52381bb741527e5
Aleen <408503598@qq.com>
2015-06-21 14:22:51 +0800

    update

--- js/music/music.js
+++ js/music/music.js
@@ -2,15 +2,26 @@ var audio = document.getElementById("audio");
 var song_name;
 if (audio != null && audio.canPlayType && audio.canPlayType("audio/mpeg"))
 {
-	var num = Math.floor(Math.random()*4);
+	
+/*	var path = location.pathname;
+	path = path.substr(0, path.lastIndexOf('/') + 1)
+	fso	= new ActiveXObject("Scripting.FileSystemObject");
+	fldr = fso.GetFolder(path);
+	fc = new Enumerator(fldr.files);
+	s = fc.item().name;*/
 	list=
 	[
 		'Mark Petrie - A New Day.mp3',
 		'Jacoo - Vacuum (Instrumental).mp3',
 		'Jacoo - Towards the Light.mp3',
-		'Jacoo - In the Shadows... Looking For a Light.mp3'
+		'Jacoo - In the Shadows... Looking For a Light.mp3',
+		'Jacoo - Nature\'s Way.mp3',
+		'Jacool - Peace Of Mind.mp3',
+		'Jacool,Oneira - Hurt.mp3'
 	]
-	load_song();
+	
+	var num = Math.floor(Math.random()*list.length);
+	load_song(0);
 	audio.addEventListener("canplaythrough", function () {
 	play();
 	}, false);
@@ -18,14 +29,21 @@ if (audio != null && audio.canPlayType && audio.canPlayType("audio/mpeg"))
 	audio.addEventListener('ended', function () {
 	// Wait 2000 milliseconds before next loop
 	setTimeout(function () { 
-	load_song();
+	load_song(1);
 	play(); }, 2000);
 	}, false);
 }
 			
          //播放器控制
 $('.next_div').click(function(){
-	load_song();
+	load_song(1);
+	audio.addEventListener("canplaythrough", function () {
+		play();
+	}, false);
+});
+
+$('.randon_div').click(function(){
+	load_song(Math.floor(Math.random()*list.length));
 	audio.addEventListener("canplaythrough", function () {
 		play();
 	}, false);
@@ -37,6 +55,7 @@ $('.audio_div').click(function(){
 	if(audio.paused) //如果当前是暂停状态
 	{
     	$('.audio_div').css("background","url(pic/pause_logo.png) no-repeat center bottom");
+		document.getElementById("play/pause").innerHTML = "<a href=\"javascript:void(0)\" title=\"Pause\"><img src=\"pic/empty.png\"></img></a>";
 		audio.play();//播放
 		return;
 	}
@@ -44,17 +63,18 @@ $('.audio_div').click(function(){
 	//当前是播放状态
         
 	$('.audio_div').css("background","url(pic/play_logo.png) no-repeat center bottom");
+	document.getElementById("play/pause").innerHTML = "<a href=\"javascript:void(0)\" title=\"Play\"><img src=\"pic/empty.png\"></img></a>";
 	audio.pause(); //暂停
 });
 		
 //加载歌
-function load_song()
+function load_song(key)
 {
+	num = (num + key) % list.length;
 	$('.song_name').css("left","1230px");
 	document.getElementById("s_name").innerHTML = "<font style=\"font-size:16px;color:#000;\">" + "Loading..." + "</font>"
 	audio.src = "bg_music/" + list[num];
 	song_name = list[num];
-	num = (num + 1) % list.length;
 }
 
 			
@@ -63,7 +83,7 @@ function play()
 {
 	audio.play();	
 	$('.song_name').css("left","1160px");
-	document.getElementById("s_name").innerHTML = "<marquee scrollamount=\"2\" style=\"font-size:16px;color:#000;\">" + "<b style=\"font-size:20px;\">" + (num == 0? list.length:num) + "/" + list.length + "</b>" + "\&nbsp;\&nbsp;\&nbsp;" + song_name.substr(0, song_name.length - (song_name.length - song_name.indexOf('.'))) + "</marquee>";
+	document.getElementById("s_name").innerHTML = "<marquee scrollamount=\"2\" style=\"font-size:16px;color:#000;\">" + "<b style=\"font-size:20px;\">" + (num + 1) + "/" + list.length + "</b>" + "\&nbsp;\&nbsp;\&nbsp;" + song_name.substr(0, song_name.length - (song_name.length - song_name.indexOf('.'))) + "</marquee>";
 	audio.volume = 0.01; 
 	setInterval(function()
 	{
