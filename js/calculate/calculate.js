dd01bb42bb804d357d60e3aa30cac72f611f378a
Aleen <aleen42@vip.qq.com>
2015-06-22 13:13:53 +0800

    update

---
+++ js/calculate/calculate.js
@@ -0,0 +1,2 @@
+var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cspan id='cnzz_stat_icon_1000477916'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s4.cnzz.com/z_stat.php%3Fid%3D1000477916%26show%3Dpic1' type='text/javascript'%3E%3C/script%3E"));
