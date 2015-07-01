07c08051765b33b87135310f87ea84b1cc4d0e0e
Aleen <aleen42@vip.qq.com>
2015-07-01 22:16:52 +0800

    update

--- example/Walkway/js/dragon.js
+++ example/Walkway/js/dragon.js
@@ -1,6 +1,6 @@
 var animation = new Walkway({
 	selector: '#loader',
-	duration: '10000',
+	duration: '4000',
 	easing: function(t){
 		return t * t;
 	}
@@ -8,3 +8,23 @@ var animation = new Walkway({
 
 animation.draw();
 
+var animation_1 = function(){
+		var t = new TimelineMax({
+			repeat: 0,
+			yoyo: false
+		});
+		var graph = document.getElementById('graph');
+		
+		t.timeScale(2);
+	
+		t.to([graph], 4, {
+			
+		});
+	
+		t.to([graph], 0.5, {
+			alpha: 1,
+			ease: Power1.easeInOut
+		});
+};
+
+animation_1();
