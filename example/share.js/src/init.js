d48cf7876c589f7aedcf01ce3d96875ff8694809
Aleen <aleen42@vip.qq.com>
2015-11-14 00:08:32 +0800

    update

--- example/share.js/src/init.js
+++ example/share.js/src/init.js
@@ -7,5 +7,6 @@ init.initShare = function () {
 	var image = 'http://share.soundtooth.cn/share.png';
 	share.initTencent('qzone', $('#share-qzone'), title, image); 
 	share.initTencent('qq', $('#share-qq'), title, image);
-	share.initWeibo('', $('#share-weibo'), title, image);  
+	share.initWeibo('', $('#share-weibo'), title, image);
+	share.initTieba($('#share-tieba'), title, image); 
 }
