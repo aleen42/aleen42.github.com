da6fcd660afdccd8214babffe4f1952f9651cee1
Aleen <aleen42@vip.qq.com>
2015-11-13 14:37:47 +0800

    update

---
+++ example/share.js/src/init.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var init = {};
+
+init.initShare = function () {
+	var title = 'Soundtooth-share.js';
+	var image = 'http://share.soundtooth.cn/share.png';
+	share.initTencent('qzone', $('#share-qzone'), title, image); 
+	share.initTencent('qq', $('#share-qq'), title, image);
+	share.initWeibo('', $('#share-weibo'), title, image);  
+}
